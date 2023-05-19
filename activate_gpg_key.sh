ddb69da32f61985920d7af296f6e6c3fb190453b
sukhman-sukh <sukhmansinghsaluja@gmail.com>
2023-05-20 00:57:11 +0530

    Signed version testing

--- activate_gpg_key.sh
+++ activate_gpg_key.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #sudo apt update
-#gpg --armor --export $GPG_KEY_ID
 #sudo apt-get install gnupg
 entry=50
 removeInput=0
@@ -14,6 +13,8 @@ do
 echo "Enter 1 to generate new gpg key "
 echo "Enter 2 to Remove Old GPG Key "
 
+echo "Enter 3 to Select GPG Key And Make It Signing key "
+
 echo "========================================="
 echo "Currently you have these keys"
 #gpg --list-secret-keys
@@ -24,30 +25,34 @@ read entry
 if [ $entry == 1 ]
 then
 	gpg --gen-key
-	echo "enter the genetated key below"
-	read GPG_KEY_ID
-	git config --global user.signingkey $GPG_KEY_ID
-	git config --global commit.gpgsign true
+	
 elif [ $entry == 2 ]
 then
-	echo "Enter 1 to Remove Old GPG Key using UID "
-	echo "Enter 2 to Remove Old GPG Key using Secret key"
+	echo "Enter 1 to Remove Old GPG Key using UID /Secret key "
 	read removeInput
 	if [ $removeInput == 1 ]
 	then
 		echo "Enter UID :- "
 		read uid
 		gpg --delete-secret-key $uid
+		gpg --delete-key $uid
 	
-	elif [ $removeInput == 2 ]
-	then
-		echo "Enter Secret Key :- "
-		read keyInput 
-		gpg --delete-secret-key $keyInput
 	else
 		echo "Wrogn entry"
 	fi
 
+elif [ $entry == 3 ]
+then
+echo "enter the Secret key You Want To Sign With , Below"
+	read GPG_KEY_ID
+	git config --global user.signingkey $GPG_KEY_ID
+	git config --global commit.gpgsign true
+	
+	echo "======================================================== \n \n"
+	
+	gpg --armor --export $GPG_KEY_ID
+	echo "\n \n ========================================================"
+	echo "Copy And Paste this Key in GitHub"
 elif [ $entry == 0 ]
 then
 echo "B-Byeee"
